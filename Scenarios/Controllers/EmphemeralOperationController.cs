49655df3ef3cebc830eb555f71eaf7050d53906b
David Fowler <davidfowl@gmail.com>
2018-10-13 09:19:29 -0700

    Added a timer leak sample

---
+++ Scenarios/Controllers/EmphemeralOperationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Scenarios.Services;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Scenarios.Controllers
+{
+    public class EmphemeralOperationController : Controller
+    {
+        [HttpGet("/timer-leak")]
+        public IActionResult Index()
+        {
+            var operation = new EphemeralOperation();
+            return Ok();
+        }
+    }
+}
---
+++ Scenarios/Services/EphemeralOperation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Scenarios.Services
+{
+    /// <summary>
+    /// Creating this object without disposing it will cause a memory leak. The object graph will look like this
+    /// Timer -> TimerHolder -> TimerQueueTimer ->  EphemeralOperation -> Timer -> ...
+    /// The timer holds onto the state which in turns holds onto the Timer, we have a circular reference.
+    /// The GC will not clean these up even if it is unreferenced. It needs to be explicitly disposed in order to avoid the leak.
+    /// </summary>
+    public class EphemeralOperation : IDisposable
+    {
+        private Timer _timer;
+
+        public EphemeralOperation()
+        {
+            _timer = new Timer(OnTimerCallback, this, 1000, 1000);
+        }
+
+        private static void OnTimerCallback(object state)
+        {
+
+        }
+
+        public void Dispose()
+        {
+            _timer.Dispose();
+        }
+    }
+}

80fc828b57d00de2635c7b3a1455290c38e13383
David Fowler <davidfowl@gmail.com>
2021-10-17 20:38:28 -0700

    Added 2 more JSON input examples

--- Scenarios/Controllers/BigJsonInputController.cs
+++ Scenarios/Controllers/BigJsonInputController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Buffers;
 using System.IO;
+using System.IO.Pipelines;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -31,8 +34,10 @@ namespace Scenarios.Controllers
         /// correct buffering on your behalf.
         /// </summary>
         [HttpPost("/big-json-input-2")]
-        public IActionResult BigJsonInput([FromBody]PokemonData rootobject)
-        {   
+        public IActionResult BigJsonInput([FromBody] PokemonData rootobject)
+        {
+            // On .NET Core 3.1/.NET 5+ this will use System.Text.Json by default which reads JSON asynchronously
+            // from the request body
             return Accepted();
         }
 
@@ -54,11 +59,64 @@ namespace Scenarios.Controllers
             var streamReader = new HttpRequestStreamReader(Request.Body, Encoding.UTF8);
 
             var jsonReader = new JsonTextReader(streamReader);
-            var serializer = new JsonSerializer();
+            var serializer = new Newtonsoft.Json.JsonSerializer();
 
             // This asynchronously reads the entire payload into a JObject then turns it into the real object.
             var obj = await JToken.ReadFromAsync(jsonReader);
             var rootobject = obj.ToObject<PokemonData>(serializer);
+
+            return Accepted();
+        }
+
+        [HttpPost("/big-json-input-5")]
+        public async Task<IActionResult> BigContentManualNewJson()
+        {
+            // This asynchronously reads the entire payload into a JObject then turns it into the real object.
+            var rootobject = await System.Text.Json.JsonSerializer.DeserializeAsync<PokemonData>(Request.Body, cancellationToken: HttpContext.RequestAborted);
+
+            return Accepted();
+        }
+
+        [HttpPost("/big-json-input-6")]
+        public async Task<IActionResult> BigContentBufferedUsingPipeReader()
+        {
+            async ValueTask<ReadResult> ReadToEndAsync()
+            {
+                var reader = Request.BodyReader;
+
+                if (Request.ContentLength is long contentLength and < int.MaxValue)
+                {
+                    return await reader.ReadAtLeastAsync((int)contentLength);
+                }
+
+                while (true)
+                {
+                    var result = await reader.ReadAsync();
+
+                    if (result.IsCompleted || result.IsCanceled)
+                    {
+                        return result;
+                    }
+
+                    reader.AdvanceTo(result.Buffer.Start, result.Buffer.End);
+                }
+            }
+
+            // Buffer the entire request body using the PipeReader. This will buffer in chunks to avoid LOH allocations
+            // and return the result as a single ReadOnlySequnce<byte>
+            var result = await ReadToEndAsync();
+            var buffer = result.Buffer;
+
+            static PokemonData ParseJson(in ReadOnlySequence<byte> data)
+            {
+                var reader = new Utf8JsonReader(data);
+                return System.Text.Json.JsonSerializer.Deserialize<PokemonData>(ref reader);
+            }
+
+            var rootObject = ParseJson(buffer);
+
+            // We've consumed the entire buffer
+            Request.BodyReader.AdvanceTo(buffer.End);
 
             return Accepted();
         }
